c3ef5f7b435564c9f93c99b1377aee64077e3817
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2023-09-18 23:03:20 +0700

    commit

---
+++ Main.java
@@ -0,0 +1,61 @@
+package encapsulation;
+
+class Pasien{
+	
+	// Set attributes
+	String nama, penyakit;
+	int umur;
+	
+	// Getter nama pasien
+	public String getNama() {
+		return nama;
+	}
+	
+	// Setter nama pasien
+	public void setNama(String newNama) {
+	    this.nama = newNama;
+	}
+	
+	// Getter nama penyakit
+	public String getPenyakit() {
+		return penyakit;
+	}
+	
+	// Setter nama penyakit
+	public void setPenyakit(String newPenyakit) {
+		this.penyakit = newPenyakit;
+	}
+	
+	// Getter umur
+	public int getUmur() {
+		System.out.print("Umur pasien : ");
+		return umur;
+	}
+	
+	// Setter public void setUmur
+	public void setUmur(int newUmur) {
+		this.umur = newUmur;
+	}
+	
+}
+
+// Main class
+class Main{
+	
+	// Main method
+	public static void main(String args[]) {
+		
+		// Init object pasien
+		Pasien Obj = new Pasien();
+		
+		// Set nama, penyakit, umur
+		Obj.setNama("Nama pasien : galih anggoro prasetya.");
+		Obj.setPenyakit("Diagnosis penyakit : tidak ada penyakit.");
+		Obj.setUmur(18);
+		
+		// Get nama, penyakit, umur
+		System.out.println(Obj.getNama());
+		System.out.println(Obj.getPenyakit());
+		System.out.println(Obj.getUmur());
+	}
+}
