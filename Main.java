510743e7d41867ba5f859cf2129010fd497fe8b3
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2023-09-23 11:10:59 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,60 +1,58 @@
+// Import paket "encapsulation"
 package encapsulation;
 
-class Pasien{
-	
-	// Set attributes
-	String nama, penyakit;
-	int umur;
-	
-	// Getter nama pasien
-	public String getNama() {
-		return nama;
-	}
-	
-	// Setter nama pasien
-	public void setNama(String newNama) {
-	    this.nama = newNama;
-	}
-	
-	// Getter nama penyakit
-	public String getPenyakit() {
-		return penyakit;
-	}
-	
-	// Setter nama penyakit
-	public void setPenyakit(String newPenyakit) {
-		this.penyakit = newPenyakit;
-	}
-	
-	// Getter umur
-	public int getUmur() {
-		return umur;
-	}
-	
-	// Setter public void setUmur
-	public void setUmur(int newUmur) {
-		this.umur = newUmur;
-	}
-	
+// Mendefinisikan kelas "Pasien"
+class Pasien {
+    // Deklarasi atribut atau variabel kelas "Pasien"
+    String nama, penyakit;
+    int umur;
+
+    // Getter nama pasien, mengembalikan nilai atribut "nama"
+    public String getNama() {
+        return nama;
+    }
+
+    // Setter nama pasien, mengatur nilai atribut "nama"
+    public void setNama(String newNama) {
+        this.nama = newNama;
+    }
+
+    // Getter nama penyakit, mengembalikan nilai atribut "penyakit"
+    public String getPenyakit() {
+        return penyakit;
+    }
+
+    // Setter nama penyakit, mengatur nilai atribut "penyakit"
+    public void setPenyakit(String newPenyakit) {
+        this.penyakit = newPenyakit;
+    }
+
+    // Getter umur, mengembalikan nilai atribut "umur"
+    public int getUmur() {
+        return umur;
+    }
+
+    // Setter umur, mengatur nilai atribut "umur"
+    public void setUmur(int newUmur) {
+        this.umur = newUmur;
+    }
 }
 
-// Main class
-class Main{
-	
-	// Main method
-	public static void main(String args[]) {
-		
-		// Init object pasien
-		Pasien Obj = new Pasien();
-		
-		// Set nama, penyakit, umur
-		Obj.setNama("Galih Anggoro Prasetya.");
-		Obj.setPenyakit("Tidak Ada Penyakit.");
-		Obj.setUmur(19);
-		
-		// Get nama, penyakit, umur
-		System.out.println(Obj.getNama());
-		System.out.println(Obj.getPenyakit());
-		System.out.println(Obj.getUmur());
-	}
+// Kelas utama "Main"
+class Main {
+    // Metode utama (main method)
+    public static void main(String args[]) {
+        // Menginisialisasi objek "Pasien" dengan nama "Obj"
+        Pasien Obj = new Pasien();
+
+        // Mengatur nilai atribut nama, penyakit, dan umur pada objek "Obj"
+        Obj.setNama("Galih Anggoro Prasetya.");
+        Obj.setPenyakit("Tidak Ada Penyakit.");
+        Obj.setUmur(19);
+
+        // Mengambil dan mencetak nilai atribut nama, penyakit, dan umur dari objek "Obj"
+        System.out.println(Obj.getNama());
+        System.out.println(Obj.getPenyakit());
+        System.out.println(Obj.getUmur());
+    }
 }
